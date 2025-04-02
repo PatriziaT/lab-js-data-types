1c93de27df52b935781f0dcbe6180adc873d8819
Patrizia Trammell <patrizia.trammell@yahoo.com>
2025-04-02 16:17:20 +0200

    done

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+//const result = s1.concat(s2, s3, s4, s5); try noticed then Im missing spaces
+const result = s1.concat(" ", s2, " ", s3, " ", s5, " ", s4);
 
 // Print out the concatenated string
-
+console.log(result);
 
 
 
@@ -23,10 +25,14 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+//const lastLetter (part1.length - 1 + part2.toUpperCase); 1st attempt
+const lastLetter1 = part1 [part1.length -1].toUpperCase();
+const lastLetter2 = part2 [part2.length -1].toUpperCase();
+//I cant think of any method out of my own head
+
 
 // Print the cameLtaiL-formatted string
-
-
+console.log(part1 + part2);
 
 
 /*******************************************
@@ -36,10 +42,16 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+//I hate math LOL
+//const perBill = (15 * 84) / 100;
+//then I checked why its wrong, and I noticed - that I cant use straight the 84 I must
+// 'always' the name of the box where its stored in. I still forget at times.
+//so if I rewrite it correctly
+const perBill = (15 * billTotal) / 100;
+
 
 // Print out the tipAmount
-
-
+console.log(perBill);
 
 
 /*******************************************
@@ -47,10 +59,13 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+//1st attempt
+//const randomNumber = Math.floor( + 1);
+//checked the syntax, Im missing the randomness
+const randomNumber = Math.floor(Math.random() *10 + 1);
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -62,15 +77,24 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+//FALSY
 
 const expression2 = a || b;
+//TRUTHY
 
 const expression3 = !a && b;
+//! = "not a"
+//does that mean that a is not an a?
 
 const expression4 = !(a && b);
+// TRUTHY
 
 const expression5 = !a || !b;
+//TRUTHY
 
 const expression6 = !(a || b);
+//FALSY
 
 const expression7 = a && a;
+//TRUTHY
+
